49548be7cf38883990a6aaa750f8a7b550194055
Jose Fernandez <jotaj1019@gmail.com>
2023-11-06 17:06:14 -0600

    commit: refactored the method 'whoWins' in src/state.ts

--- src/state.ts
+++ src/state.ts
@@ -1,91 +1,85 @@
 type Plays = "rock" | "paper" | "scissors";
 
 type Game = {
-    myGame: Plays,
-    computerGame: Plays
-} 
+    myGame: Plays;
+    computerGame: Plays;
+};
 
 const state = {
     data: {
-        currentGame: { 
+        currentGame: {
             myGame: "",
-            computerGame: ""
+            computerGame: "",
         },
-        history:[],
-        userName: ""
+        history: [],
+        userName: "",
     },
 
     listeners: [],
 
-    getState(){
+    getState() {
         return this.data;
     },
 
-    setState(newState){
+    setState(newState) {
         this.data = newState;
-        for (const cb of this.listeners){
+        for (const cb of this.listeners) {
             cb();
             this.listeners = [];
         }
         localStorage.setItem("saved-game", JSON.stringify(newState));
     },
 
-    subscribe(callBack: (any)=>any){
+    subscribe(callBack: (any) => any) {
         this.listeners.push(callBack);
     },
 
-    initOfHistory(){
+    initOfHistory() {
         const dataOfHistory: any = localStorage.getItem("saved-game");
-        if(dataOfHistory !== null){
-            this.setState(JSON.parse(dataOfHistory))
-        }   
+        if (dataOfHistory !== null) {
+            this.setState(JSON.parse(dataOfHistory));
+        }
     },
 
-    pushToHistory(game: Game){
+    pushToHistory(game: Game) {
         const currentState = this.getState();
         currentState.history.push(game);
         this.setState(currentState);
     },
 
-    setMove(myMove: Plays, computerMove: Plays){
+    setMove(myMove: Plays, computerMove: Plays) {
         const currentState = this.getState();
         currentState.currentGame.myGame = myMove;
         currentState.currentGame.computerGame = computerMove;
-        this.setState(currentState)
-        this.pushToHistory({myGame: myMove, computerGame: computerMove})
+        this.setState(currentState);
+        this.pushToHistory({ myGame: myMove, computerGame: computerMove });
     },
 
-    whoWins(myPlay: Plays, computerPlay: Plays){
+    whoWins(myPlay: Plays, computerPlay: Plays) {
         const winner = "winner";
         const loser = "loser";
         const tied = "tied";
-        
-        const wonWithScissors: boolean = myPlay === "scissors" && computerPlay === "paper";
-        const wonWithPaper: boolean = myPlay === "paper" && computerPlay === "rock";
-        const wonWithRock: boolean = myPlay === "rock" && computerPlay === "scissors";
 
-        const lostWithScissors: boolean = myPlay === "scissors" && computerPlay === "rock";
-        const lostWithPaper: boolean = myPlay === "paper" && computerPlay === "scissors";
-        const lostWithRock: boolean = myPlay === "rock" && computerPlay === "paper";
-
-        const tiedWithScissors: boolean = myPlay === "scissors" && computerPlay === "scissors";
-        const tiedWithPaper: boolean = myPlay === "paper" && computerPlay === "paper";
-        const tiedWithRock: boolean = myPlay === "rock" && computerPlay === "rock";
-
-        const victory: boolean = [wonWithScissors, wonWithRock, wonWithPaper].includes(true);
-        const defeat: boolean = [lostWithScissors, lostWithRock, lostWithPaper].includes(true);
-        const tie: boolean = [tiedWithScissors, tiedWithRock, tiedWithPaper].includes(true);
-
-        if(victory){
-            return winner;
+        const gameRules = {
+            paper: {
+                paper: tied,
+                rock: winner,
+                scissors: loser,
+            },
+            rock: {
+                scissors: winner,
+                rock: tied,
+                paper: loser,
+            },
+            scissors: {
+                paper: winner,
+                scissors: tied,
+                rock: loser,
+            },
         };
-        if(defeat){
-            return loser;
-        };
-        if(tie){
-            return tied;
-        };
+
+        return gameRules[myPlay][computerPlay];
     },
-}
+};
 
-export { state }
+export { state };
